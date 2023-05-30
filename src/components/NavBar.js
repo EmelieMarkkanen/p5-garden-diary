8adb0180bd3b0065d70e54f37e4a53ad6fdbd5a0
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-05-30 12:59:43 +0000

    Add Avatar.js component with css to display user profile image, add links to navbar with signout functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,57 @@ import { Navbar, Container, Nav, Image } from "react-bootstrap";
 import logo from "../assets/Plant logo small.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
+
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+    }
+  };
+
+
+  const addPostIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/posts/create"
+    >Add post
+    </NavLink>
+  );
+
+  const loggedInIcons = <>
+   <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/feed"
+      >Followed
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/liked"
+      >Liked
+      </NavLink>
+      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>Sign out
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+      </NavLink>
+
+  </>;
+
   const loggedOutIcons = (
     <>
       <NavLink
@@ -34,7 +79,7 @@ const NavBar = () => {
           <Image src={logo} roundedCircle alt='logo' height={45} className={styles.Logo}/>
           </Navbar.Brand>
         </NavLink>
-
+        {currentUser && addPostIcon}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
