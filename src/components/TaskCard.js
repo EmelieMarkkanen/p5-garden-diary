91fcb8bb3617eb58c5f32952939a95e07bc84169
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-06-05 21:13:28 +0000

    Update TaskCard do display icon if task is overdue

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -2,13 +2,30 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import { useHistory } from "react-router-dom";
 import styles from "../styles/Cards.module.css"
+import { useEffect } from "react";
+import { axiosReq } from "../api/axiosDefaults";
 
-function TaskCard({ task }) {
+function TaskCard({ task, setTask }) {
   const history = useHistory();
 
   const handleClick = () => {
     history.push(`/tasks/${task.id}`);
   };
+
+  useEffect(() => {
+    const updateOverdue = async () => {
+      try {
+        const { data } = await axiosReq.patch(`/tasks/${task.id}/`, {
+          overdue: task.is_overdue,
+        });
+        setTask(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    updateOverdue();
+  }, [task, setTask]);
 
   return (
     <Card onClick={handleClick}>
@@ -22,9 +39,13 @@ function TaskCard({ task }) {
         )}
       </div>
       <Card.Body>
-        <Card.Title>{task.title}</Card.Title>
+        <Card.Title>
+          {task.title}
+        </Card.Title>
         <Card.Text>Added: {task.created_at}</Card.Text>
-        <Card.Text>Due: {task.due_date}</Card.Text>
+        <Card.Text>{task.overdue ? (
+              <i className="fas fa-exclamation text-danger text-bold"></i>
+          ) : null} Due: {task.due_date}</Card.Text>
       </Card.Body>
     </Card>
   );
