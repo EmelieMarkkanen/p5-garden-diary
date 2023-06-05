d7eb1450c121c42a4179b54caf4c277da07a2c1f
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-06-05 10:35:16 +0000

    Add pppular profiles to PostPage.js

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../../styles/Post.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
 import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 import Avatar from "../../components/Avatar";
